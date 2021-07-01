6460cea650ced5f224c08f02a7d4634cc5498ced
Samantha Sommer <samanthajeddy@gmail.com>
2021-07-01 14:45:18 -0700

    hw

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,11 +1,11 @@
 // @TODO: YOUR CODE HERE!
 var svgWidth = 960;
-var svgHeight = 500;
+var svgHeight = 625;
 
 var margin = {
   top: 20,
   right: 40,
-  bottom: 200,
+  bottom: 100,
   left: 100
 };
 
@@ -13,13 +13,14 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold chart, and shift the latter by left and top margins.
-var chart = d3.select("#scatter")
-  .append("div")
-  .classed("chart", true);
-
-  var svg = chart.append("svg")
+var svg = d3.select("#scatter")
+  .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
+
+  //var svg = chart.append("svg")
+  //.attr("width", svgWidth)
+  //.attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -29,17 +30,17 @@ d3.csv("data.csv").then(function(data) {
     // Step 1: Parse Data/Cast as numbers
     // ==============================
     data.forEach(function(data) {
-        data.healthcare = +data.healthcare;
         data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
       });
  // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(data, d => d.healthcare)])
+      .domain([20, d3.max(data, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.poverty)])
+      .domain([0, d3.max(data, d => d.healthcare)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -61,10 +62,10 @@ d3.csv("data.csv").then(function(data) {
       .data(data)
       .enter()
       .append("circle")
-      .attr("cx", d => xLinearScale(d.healthcare))
-      .attr("cy", d => yLinearScale(d.poverty))
+      .attr("cx", d => xLinearScale(d.poverty +1.5))
+      .attr("cy", d => yLinearScale(d.healthcare+.3))
       .attr("r", "15")
-      .attr("fill", "pink")
+      .attr("fill", "blue")
       .attr("opacity", ".5");
   
   
@@ -74,7 +75,7 @@ d3.csv("data.csv").then(function(data) {
       .attr("class", "tooltip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.state}<br>Hair length: ${d.healthcare}<br>Hits: ${d.poverty}`);
+        return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
       });
 
     // Step 7: Create tooltip in the chart
@@ -91,19 +92,23 @@ d3.csv("data.csv").then(function(data) {
         toolTip.hide(data);
       });
 
+      
+      
     // Create axes labels
+   
+
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Healthcare vs Poverty");
+      .text("Lacks Healthcare(%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Healthcare");
+      .text("In Poverty (%)");
   }).catch(function(error) {
     console.log(error);
   });
